53d37c0af8be35cb7464bcc5267867d8fdc59408
ShiddiqRija <shiddiq100@gmail.com>
2023-07-18 14:26:05 +0700

    add send message to traccar

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\MessageRequest;
 use App\Models\Device;
 use App\Models\Message;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
@@ -25,17 +26,52 @@ class MessageController extends Controller
 
     public function store(MessageRequest $request)
     {
-        $validation = $request->validated();
-        $validation['unique_id']    = Device::where('device_id', $validation['device_id'])->first()->unique_id;
-        $validation['send_time']    = round(microtime(true) * 1000);
-        $validation['created_by']   = auth()->user()->id;
+        try {
+            $validation = $request->validated();
+            $validation['unique_id']    = Device::where('device_id', $validation['device_id'])->first()->unique_id;
+            $validation['send_time']    = round(microtime(true) * 1000);
+            $validation['created_by']   = auth()->user()->id;
 
-        DB::beginTransaction();
+            $sendMessage = $this->send($validation);
+            $status = json_decode($sendMessage->getStatusCode());
 
-        Message::create($validation);
+            if ($status == 200 || $status == 202) {
+                DB::beginTransaction();
+        
+                Message::create($validation);
+        
+                DB::commit();
+        
+                return Redirect::route('messages.index');
+            }
+        } catch (\Exception $err) {
+            DB::rollBack();
 
-        DB::commit();
+            return Redirect::back()->withErrors(['error' => $err->getMessage()]);
+        }
+    }
 
-        return Redirect::route('messages.index');
+    public function send($data)
+    {
+        $client = new Client();
+
+        $res = $client->request('POST', env('GPS_SERVER') . '/api/commands/send', [
+            'auth' => [
+                env('GPS_USER_EMAIL'), env('GPS_USER_PASSWORD')
+            ],
+            'headers' => [
+                'Content-Type'  => 'application/json'
+            ],
+            'body' => json_encode([
+                'deviceId'      => $data['device_id'],
+                'description'   => 'Message from Tracking App Client',
+                'type'          => 'custom',
+                'attributes'    => [
+                    'data'  => 'mg=' . $data["message"]
+                ]
+            ])
+        ]);
+
+        return $res;
     }
 }
