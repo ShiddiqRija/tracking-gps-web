1628dfbe5c0661f0a79954f049c7595f54ed9629
ShiddiqRija <shiddiq100@gmail.com>
2023-08-14 11:13:51 +0700

    Fix: add build and esbuild to handle top-level-await for map

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        target: "es2022",
+    },
+    esbuild: {
+        supported: {
+            "top-level-await": true,
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
